afd664e83b90a75383239bc0157652ef4fbd51cf
jrl1373 <jrl1373@rit.edu>
2025-02-16 21:46:32 -0500

    projectile

---
+++ Src/SymbolMaker/projectiles/line_projectile.gd
@@ -0,0 +1,26 @@
+extends Node2D
+@export var length: int
+@export var velocity: float
+@export var heading: float
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	init()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:	
+	self.position += Vector2(0,-1).rotated(deg_to_rad(heading)) * (velocity * delta)
+	pass
+
+
+func init():
+	var line = Line2D.new()
+	line.width = 10
+	line.add_point(Vector2(-length/2,0))
+	line.add_point(Vector2(length/2,0))
+	self.add_child(line)
+	self.rotate(deg_to_rad(heading))
+	$Area2D/CollisionShape2D.shape.size.x = length
